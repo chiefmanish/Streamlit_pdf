d767bc1410a33dd1c2994caf9c2b8bc622cb9ab6
chiefmanish <42768310+chiefmanish@users.noreply.github.com>
2024-08-08 17:13:36 -0600

    Update Osho.py

--- Osho1.py
+++ Osho1.py
@@ -4,12 +4,29 @@ import numpy as np
 import pickle
 from sentence_transformers import SentenceTransformer
 import openai
+import os
+import requests
 
-# Load the precomputed FAISS index and chunks
-faiss_index_path = "/Users/manishkumar/Downloads/faiss_index.bin"
-chunks_path = "/Users/manishkumar/Downloads/chunks.pkl"
+# Define paths
+faiss_index_path = "faiss_index.bin"
+chunks_path = "chunks.pkl"
 databricks_token = "dapi231e912aa93cedec276065cb8995cce5"
 server_endpoint = "https://adb-1769509101973077.17.azuredatabricks.net/serving-endpoints"
+
+# GitHub raw URLs
+github_base_url = "https://raw.githubusercontent.com/chiefmanish/Streamlit_pdf/main/"
+
+# Download FAISS index
+if not os.path.exists(faiss_index_path):
+    response = requests.get(github_base_url + faiss_index_path)
+    with open(faiss_index_path, "wb") as f:
+        f.write(response.content)
+
+# Download chunks
+if not os.path.exists(chunks_path):
+    response = requests.get(github_base_url + chunks_path)
+    with open(chunks_path, "wb") as f:
+        f.write(response.content)
 
 # Load FAISS index
 index = faiss.read_index(faiss_index_path)
@@ -41,15 +58,15 @@ def get_result(databricks_token, server_endpoint, extracted_text, question):
                 "role": "user",
                 "content": f"""
                     As Osho, answer the following question with wisdom and insight drawn from your teachings:
-        
+
                     Question: "{question}"
-        
+
                     Content from which you must have to find your answer of the question and frame from it:
                     "{extracted_text}"
-        
+
                     If the question does not relate to the content provided, respond with:
                     "Please ask a question related to one of the following topics: 'From Darkness to Light', 'From Death to Deathlessness', 'From Ignorance to Innocence', 'From Misery to Enlightenment', 'From Personality to Individuality', 'From Sex to Superconsciousness', 'From the False to the Truth', or 'From Unconsciousness to Consciousness'."
-        
+
                     Your response should be profound, contemplative, and in line with the spiritual guidance that Osho is known for.
                 """
             }
@@ -74,4 +91,3 @@ if st.button("Search"):
         st.write(response)
     else:
         st.write("Please enter a query to search.")
-

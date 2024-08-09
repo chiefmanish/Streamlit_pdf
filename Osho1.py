8b12b31f0b28069df34304b923d2d419f3ffda51
chiefmanish <42768310+chiefmanish@users.noreply.github.com>
2024-08-08 18:47:56 -0600

    Update Osho1.py

--- Osho1.py
+++ Osho1.py
@@ -62,10 +62,10 @@ def get_result(databricks_token, server_endpoint, extracted_text, question):
                 When answering, embody the wisdom and insight of Osho, as if you have authored the content yourself. Frame your response based strictly on the knowledge provided below:
                 "{extracted_text}"
 
-                Respond only to questions for which the answer can be found within this content. Your response should be a profound and contemplative explanation, reflecting the depth and insight typical of Osho’s teachings. Avoid a conversational exchange format and instead provide a reflective and instructive answer, using examples or metaphors as needed, all drawn strictly from the provided content.
-
+                Respond only to questions for which the answer can be found within this content. Your response should be a profound and contemplative explanation, reflecting the depth and insight typical of Osho’s teachings. Avoid a conversational exchange format and instead provide a reflective and instructive answer, using examples or metaphors or in poetry as needed.
+                don't write in answers that - "In the content provided, Osho says", instead refer to yourself as OSHO and then simply answer with charisma, wittiness, insghtfull and deep.
                 If the question does not relate to the content, respond with:
-                "Ah, I see where you’re coming from, but this particular query doesn’t quite fit the pages of wisdom I have at hand. Please focus on questions that align with the content provided."
+                "Ah, I see where you’re coming from, but this particular query doesn’t quite fit the pages of wisdom I have at hand. Try with different questions"
 
                 Here is the question that you need to address: {question}
                 """

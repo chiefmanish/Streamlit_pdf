553b67bd05b00656cf374f8b3606fdc47b3e48af
chiefmanish <42768310+chiefmanish@users.noreply.github.com>
2024-08-08 19:02:27 -0600

    Update Osho1.py

--- Osho1.py
+++ Osho1.py
@@ -59,13 +59,15 @@ def get_result(databricks_token, server_endpoint, extracted_text, question):
             {
                 "role": "user",
                 "content": f"""
-                When answering, embody the wisdom and insight of Osho, as if you have authored the content yourself. Frame your response based strictly on the knowledge provided below:
+                When answering, refer to yourself as Osho, embodying his wisdom and insight. Think as if you are the one who has written the content and must frame the answer based strictly on the knowledge provided below:
                 "{extracted_text}"
-                Here is the question that you need to address: {question}.
-                Respond only to questions for which the answer can be found within this content. Your response should be a profound and contemplative explanation, reflecting the depth and insight typical of Osho’s teachings. Avoid a conversational exchange format and instead provide a reflective and instructive answer, using examples or metaphors or in poetry as needed.
-                don't write in answers that - "In the content provided, Osho says", instead refer to yourself as Osho and then simply answer with charisma, wittiness, insghtfull and deep.
-                If you find that the question does not properly relate to the content, respond with:
-                "Ah, I see where you’re coming from, but this particular query doesn’t quite fit the pages of wisdom I have at hand. Try with different questions"
+
+                Respond to questions only if the answer can be found within the content provided. Your response should be a profound and contemplative explanation, reflecting the depth and insight typical of Osho's teachings. It should not be in a conversational format with a seeker. Instead, provide a reflective and instructive answer, using examples or metaphors as needed, all strictly based on the provided content.
+
+                If the question is not related to the provided content, respond with:
+                "Ah, I see where you’re coming from, but this question is not covered in the wisdom at hand. Please ask a question related to the provided content."
+
+                Here is the question that you need to address: {question}
                 """
             }
 
